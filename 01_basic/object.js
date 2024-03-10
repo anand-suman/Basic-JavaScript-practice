5ad2cafa9b752d6c146a387ff8e6ad092abb7a8e
Anand Kumar Suman <123244254+anand-suman@users.noreply.github.com>
2024-03-10 05:52:30 +0000

    Object

--- 01_basic/object.js
+++ 01_basic/object.js
@@ -9,6 +9,7 @@ const mySym2 = Symbol("key2")
 const JsUser = {
     name: "Anand",
     "full name": "Anand suman",
+    [mySym] : "My symbol",
     age: 23,
     location: "Hajipur",
     email: "anandraj@google.com",
@@ -16,9 +17,28 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 } // object 
 
-console.log(JsUser.email);
-console.log(JsUser.name);
-console.log(JsUser["full name"]);
-console.log(JsUser["email"])
+// console.log(JsUser.email);
+// console.log(JsUser.name);
+// console.log(JsUser["full name"]);
+// console.log(JsUser["email"])
+// console.log(JsUser[mySym]);
+JsUser.email = "anand@hotmail.com"// for change the object content of the difined content
+//Object.freeze(JsUser)// to freez the objects it meaning is that we cant change the objects value
+JsUser.name = "Chhotu"
+// console.log(JsUser.name);
+// console.log(JsUser.email);
+// console.log(JsUser);
+
 // console.log(JsUser["email"])
 
+
+// ============================================= fuction ======================================
+
+JsUser.greeting = function(){
+    console.log("hello js user")
+}
+JsUser.greeting_2 = function(){
+    console.log(`Hello ${this.name} bhaiya`)
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greeting_2());
